ddd949ae769dfb78d2a7e1e47f3fb8bd2aad47bd
Zach Bresnick <zbresnick@ayzenberg.com>
2016-05-26 18:45:14 -0700

    add aws lambda code

--- deploy_to_lambda.sh
+++ deploy_to_lambda.sh
@@ -4,9 +4,7 @@ echo "hello world"
 
 zip -r LambdaTest.zip index.js
 
-aws lambda list-functions
-
-#aws lambda update-function-code --function-name LambdaTest --zip-file fileb://LambdaTest.zip
+aws lambda update-function-code --function-name continuousIntegrationTest --zip-file fileb://LambdaTest.zip
 
 # Publishing a new Version of the Lambda function
 #version=`aws lambda publish-version --function-name LambdaTest | jq -r .Version`
