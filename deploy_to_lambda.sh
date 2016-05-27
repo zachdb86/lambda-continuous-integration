b832c2688375291e21eb8f507687d827e62d0c98
Zach Bresnick <zbresnick@ayzenberg.com>
2016-05-26 18:58:45 -0700

    Add versioning to deploy_to_lambda.sh

--- deploy_to_lambda.sh
+++ deploy_to_lambda.sh
@@ -2,19 +2,19 @@ pip install awscli
 
 echo "hello world"
 
+# Preparing and deploying Function to Lambda
 zip -r LambdaTest.zip index.js
-
 aws lambda update-function-code --function-name continuousIntegrationTest --zip-file fileb://LambdaTest.zip
 
 # Publishing a new Version of the Lambda function
-#version=`aws lambda publish-version --function-name LambdaTest | jq -r .Version`
+version=`aws lambda publish-version --function-name LambdaTest | jq -r .Version`
 
 # Updating the PROD Lambda Alias so it points to the new function
-#aws lambda update-alias --function-name LambdaTest --function-version $version --name PROD
+aws lambda update-alias --function-name continuousIntegrationTest --function-version $version --name PROD
 
-#aws lambda get-function --function-name “LambdaTest”
+aws lambda get-function --function-name “continuousIntegrationTest”
 
 # Invoking Lambda function from update PROD alias
-#aws lambda invoke --function-name LambdaTest --payload "$(cat data.json)" --qualifier PROD lambda_output.txt
+aws lambda invoke --function-name continuousIntegrationTest --payload "$(cat data.json)" --qualifier PROD lambda_output.txt
 
-#cat lambda_output.txt
+cat lambda_output.txt
