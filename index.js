1118d5bee8533be72ddb1fa8df31b85ba248b1b4
Zach Bresnick <zbresnick@ayzenberg.com>
2016-05-27 10:50:39 -0700

    test

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.handler = function(event, context){
     console.log('value1 =', event.key1);
     console.log('value2 =', event.key2);
     console.log('value3 =', event.key3);
-    context.succeed(event.key1);
+    context.succeed(event.key1 + " this is working");
     context.fail('Somthing went wrong');
 };
