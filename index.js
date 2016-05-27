9aa318e9de15c6ca6ddfb3f0626eab18f7b6e3c9
Zach Bresnick <zbresnick@ayzenberg.com>
2016-05-26 18:06:43 -0700

    npm init

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Loading function');
+
+exports.handler=(event, context){
+    console.log('value1 =', event.key1);
+    console.log('value2 =', event.key2);
+    console.log('value3 =', event.key3);
+    context.succeed(event.key1);
+    context.fail('Somthing went wrong');
+};
