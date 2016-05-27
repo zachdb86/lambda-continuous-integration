412d975de68ccc5732f4ae6989daf098213460da
Zach Bresnick <zbresnick@ayzenberg.com>
2016-05-26 18:53:40 -0700

    add data.json for testing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('Loading function');
 
-exports.handler=(event, context){
+exports.handler = function(event, context){
     console.log('value1 =', event.key1);
     console.log('value2 =', event.key2);
     console.log('value3 =', event.key3);
