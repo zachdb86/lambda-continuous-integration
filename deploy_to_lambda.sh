0c334f9d0a904017d2cf423bb1d33710a023b4be
Zach Bresnick <zbresnick@ayzenberg.com>
2016-05-26 19:00:55 -0700

    fix typo

--- deploy_to_lambda.sh
+++ deploy_to_lambda.sh
@@ -7,7 +7,7 @@ zip -r LambdaTest.zip index.js
 aws lambda update-function-code --function-name continuousIntegrationTest --zip-file fileb://LambdaTest.zip
 
 # Publishing a new Version of the Lambda function
-version=`aws lambda publish-version --function-name LambdaTest | jq -r .Version`
+version=`aws lambda publish-version --function-name continuousIntegrationTest | jq -r .Version`
 
 # Updating the PROD Lambda Alias so it points to the new function
 aws lambda update-alias --function-name continuousIntegrationTest --function-version $version --name PROD
