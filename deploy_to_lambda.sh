6b43f7a358cd856ec84b8f32da86a2716f85d1f3
Zach Bresnick <zbresnick@ayzenberg.com>
2016-05-26 18:40:02 -0700

    Update deploy_to_lambda.sh

--- deploy_to_lambda.sh
+++ deploy_to_lambda.sh
@@ -1,1 +1,22 @@
+pip install awscli
+
 echo "hello world"
+
+zip -r LambdaTest.zip index.js
+
+aws lambda list-functions
+
+#aws lambda update-function-code --function-name LambdaTest --zip-file fileb://LambdaTest.zip
+
+# Publishing a new Version of the Lambda function
+#version=`aws lambda publish-version --function-name LambdaTest | jq -r .Version`
+
+# Updating the PROD Lambda Alias so it points to the new function
+#aws lambda update-alias --function-name LambdaTest --function-version $version --name PROD
+
+#aws lambda get-function --function-name “LambdaTest”
+
+# Invoking Lambda function from update PROD alias
+#aws lambda invoke --function-name LambdaTest --payload "$(cat data.json)" --qualifier PROD lambda_output.txt
+
+#cat lambda_output.txt
